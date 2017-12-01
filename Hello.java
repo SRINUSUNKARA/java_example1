3d9138ffa24d94b1fc1b8b81f0a07e71788f1d4c
SRINUSUNKARA <srenu.satyavani@gmail.com>
2017-12-01 09:07:38 +0530

    first commit with modification

--- Hello.java
+++ Hello.java
@@ -4,6 +4,6 @@ class Hello
 {
    public static void main(String args[])
    {
-      System.out.println("Hello Guys good day and welcome to DevOps session");
+      System.out.println("Hello Guys good day and welcome to DevOps session 8:30 AM");
    }
 }
